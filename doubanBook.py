05f18dfac5be5846518fddd04dc08489693913d2
dangsh <954316227@qq.com>
2017-11-10 17:46:38 +0800

    add a branch trendhu

--- doubanBook.py
+++ doubanBook.py
@@ -9,15 +9,17 @@ for i in range(10):
     soup = BeautifulSoup(res.text , 'html.parser') 
 
     for news in soup.select('.item'): #定位
-        person = news.find_all(class_='pl')[1].text 
+        person = news.find_all(class_='pl')[1].text.replace(' ','')
+        title = news.select('a')[1].text.replace(' ','')
 
         jianjie = ""
         try:
             jianjie = news.find_all(class_='inq')[0].text 
         except:
             pass;
-            
-        newsary.append({'title':news.select('a')[1].text , 'name':news.select('p')[0].text , 'person':person , 'jianjie': jianjie})
+        
+        name = news.select('p')[0].text
+        newsary.append({'title': title , 'name': name , 'person':person , 'jianjie': jianjie})
 
 newsdf = pandas.DataFrame(newsary)
 newsdf.to_excel('doubanbook1.xlsx')
