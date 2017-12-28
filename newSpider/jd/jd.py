80de93bf3b5deb9d58ddda06bdfc457d8910df05
dangsh <954316227@qq.com>
2017-12-28 21:33:39 +0800

    update jd.py

--- newSpider/jd/jd.py
+++ newSpider/jd/jd.py
@@ -13,38 +13,28 @@ def login(b):  #登录京东
     b.find_by_id("loginsubmit").click()
     time.sleep(3)
     return b
-
-#订单页
-def loop(b):  #循环点击
-    try:
-        if b.title=="订单结算页 -京东商城":
-            b.find_by_text("保存收货人信息").click()
-            b.find_by_text("保存支付及配送方式").click()
-            b.find_by_id("order-submit").click()
-            return b
-        else:  #多次抢购操作后，有可能会被转到京东首页，所以要再打开手机主页
-            b.visit("https://item.jd.com/4993737.html")
-            b.find_by_id("choose-btn-qiang").click()
-            time.sleep(10)
-            loop(b)  #递归操作
-    except Exception as e: #异常情况处理，以免中断程序
-        b.reload()  #重新刷新当前页面，此页面为订单提交页
-        time.sleep(2)
-        loop(b)  #重新调用自己
-
+    
 
 b=Browser(driver_name="chrome") #打开浏览器
-b.visit("https://item.jd.com/4993737.html")
+b.visit("https://item.jd.com/5853593.html")
 login(b)
-b.find_by_id("choose-btn-qiang").click() #找到抢购按钮，点击
-time.sleep(10)  #等待10sec
+
 while True:
-    loop(b)
-    if b.is_element_present_by_id("tryBtn"): #订单提交后显示“再次抢购”的话
-        b.find_by_id("tryBtn").click()  #点击再次抢购，进入读秒5，跳转订单页
-        time.sleep(6.5)
-    elif b.title=="订单结算页 -京东商城": #如果还在订单结算页
-        b.find_by_id("order-submit").click() 
-    else:
-        print('恭喜你，抢购成功')
-        break
+    try:
+        b.click_link_by_text("立即抢购")
+        try:
+            if b.title=="商品已成功加入购物车":
+                print("加入购物车成功")
+                break
+            else:  #多次抢购操作后，有可能会被转到京东首页，所以要再打开手机主页
+                b.visit("https://item.jd.com/5853593.html")
+                b.click_link_by_text("立即抢购")
+                time.sleep(1)
+                break
+        except Exception as e: #异常情况处理，以免中断程序
+            b.reload() 
+            time.sleep(2)
+            break
+            
+    except:
+        print("没找到抢购按钮")
