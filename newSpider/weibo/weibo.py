77d631ba8787ec2d99bfd380364cea5a84f3a387
dangsh <954316227@qq.com>
2017-12-30 19:10:34 +0800

    update the weibo.py

--- newSpider/weibo/weibo.py
+++ newSpider/weibo/weibo.py
@@ -1,15 +1,37 @@
 from splinter.browser import Browser
+from selenium import webdriver
 import time
 
-def login(b):
-    print("login")
+# b = Browser(driver_name = 'chrome')
+# b.visit("https://weibo.com/")
+# time.sleep(10)
+# b.fill('username' , '123')
+# b.fill('password' , '123')
+# b.click_link_by_text("你好，请登录")
 
+browser = webdriver.Chrome()
+#设置浏览器加载超时时间
+browser.set_page_load_timeout(30)
+loginUrl = "https://weibo.com/"
+browser.get(loginUrl)
 
+try:
+    browser.find_element_by_xpath('//*[@name="username"]').send_keys('13096925043')
+    print('user success!')
+except:
+    print('user error!')
+time.sleep(1)
 
-b = Browser(driver_name = 'chrome')
-b.visit("https://weibo.com/")
-time.sleep(10)
-b.fill('username' , '123')
-b.fill('password' , '123')
-b.click_link_by_text("登录")
+try:
+    browser.find_element_by_xpath('//*[@name="password"]').send_keys('c2ptymvr')
+    print('pw success!')
+except:
+    print('pw error!')
+time.sleep(1)
 
+try:
+    browser.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click()
+    print('click success!')
+except:
+    print('click error!')
+time.sleep(30)
