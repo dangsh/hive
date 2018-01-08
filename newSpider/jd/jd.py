25aec5556e3c691212cab973ad97c7d223c70a84
dangsh <954316227@qq.com>
2018-01-08 22:06:46 +0800

    update jd.py in 1_8 better than before

--- newSpider/jd/jd.py
+++ newSpider/jd/jd.py
@@ -13,28 +13,26 @@ def login(b):  #登录京东
     b.find_by_id("loginsubmit").click()
     time.sleep(3)
     return b
-    
 
-b=Browser(driver_name="chrome") #打开浏览器
-b.visit("https://item.jd.com/5853593.html")
-login(b)
-
-while True:
+def loop(b):
     try:
         b.click_link_by_text("立即抢购")
-        try:
-            if b.title=="商品已成功加入购物车":
-                print("加入购物车成功")
-                break
-            else:  #多次抢购操作后，有可能会被转到京东首页，所以要再打开手机主页
-                b.visit("https://item.jd.com/5853593.html")
-                b.click_link_by_text("立即抢购")
-                time.sleep(1)
-                break
-        except Exception as e: #异常情况处理，以免中断程序
-            b.reload() 
-            time.sleep(2)
-            break
-            
+        if b.title=="商品已成功加入购物车":
+            print("加入购物车成功")
+        else:  #多次抢购操作后，有可能会被转到京东首页，所以要再打开手机主页
+            b.visit("https://item.jd.com/4993737.html")
+            b.click_link_by_text("立即抢购")
+            time.sleep(1)
+        
     except:
         print("没找到抢购按钮")
+        time.sleep(1)
+        loop(b)
+
+b=Browser(driver_name="chrome") #打开浏览器
+b.visit("https://item.jd.com/4993737.html")
+login(b)
+time.sleep(2)
+loop(b)
+
+
