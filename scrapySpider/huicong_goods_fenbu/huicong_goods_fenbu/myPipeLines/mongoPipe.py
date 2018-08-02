1f04d54d7400b29167fc666ba8a2f829948ea5b0
dangsh <954316227@qq.com>
2018-08-02 14:04:56 +0800

    update in 8_2

--- scrapySpider/huicong_goods_fenbu/huicong_goods_fenbu/myPipeLines/mongoPipe.py
+++ scrapySpider/huicong_goods_fenbu/huicong_goods_fenbu/myPipeLines/mongoPipe.py
@@ -6,27 +6,44 @@ from gcpy_utils.spider_utils import  sync_dataflow_push
 
 class MongopipClass(object):
 
-    def __init__(self):
-        client = MongoClient(settings["MONGO_HOST"] , settings["MONGO_PORT"])
-        myDb = client[settings["MONGO_DBNAME"]]
-        self.myCollection = myDb[settings["MONGO_COLLECTION"]]
-        self.myCollection2 = myDb["huicong_com2"]
+    # def __init__(self):
+    #     client = MongoClient(settings["MONGO_HOST"] , settings["MONGO_PORT"])
+    #     myDb = client[settings["MONGO_DBNAME"]]
+    #     self.myCollection = myDb[settings["MONGO_COLLECTION"]]
+    #     self.myCollection2 = myDb["huicong_com2"]
 
     def process_item(self, item, spider):
         #如果com_data 不存在，则只处理goods_data
+        # if not item["com_data"]:
+        #     try:
+        #         self.myCollection.insert([item["goods_data"]])
+        #     except:
+        #         pass
+        # else:
+        #     try:
+        #         self.myCollection.insert([item["goods_data"]])
+        #     except:
+        #         pass
+        #     try:
+        #         self.myCollection2.insert([item["com_data"]])
+        #     except:
+        #         pass
         if not item["com_data"]:
             try:
-                self.myCollection.insert([item["goods_data"]])
+                sync_dataflow_push.dataflow_push("hc360_product", item["goods_data"]["source_url"], item["goods_data"])
             except:
+                print("update goods error1")
                 pass
         else:
             try:
-                self.myCollection.insert([item["goods_data"]])
+                sync_dataflow_push.dataflow_push("hc360_product", item["goods_data"]["source_url"], item["goods_data"])
             except:
+                print("update goods error2")
                 pass
             try:
-                self.myCollection2.insert([item["com_data"]])
+                sync_dataflow_push.dataflow_push("hc360_company", item["com_data"]["source_url"], item["com_data"])
             except:
+                print("update goods error2")
                 pass
 
         return item
