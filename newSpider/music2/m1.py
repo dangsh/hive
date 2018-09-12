a4bf9a47bc4a4f43fee7e079156867113cacba49
dangsh <954316227@qq.com>
2018-09-12 17:10:40 +0800

    update music, add proxy

--- newSpider/music2/m1.py
+++ newSpider/music2/m1.py
@@ -1,23 +1,24 @@
-# from splinter.browser import Browser
-# import time
-#
-# b = Browser(driver_name = 'chrome')
-# b.visit("https://music.163.com/#/song?id=1307440707")
-# time.sleep(5)
-# b.find_by_xpath('//*[@id="content-operation"]/a[1]').first().click()
-# time.sleep(3)
-# b.quit()
-
 from selenium import webdriver
 import  time
 from selenium.webdriver.chrome.options import Options
+from gcpy_utils.proxy import adsl
+import random
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--headless')
+# chrome_options = Options()
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_argument('--headless')
 for i in range(10000):
     try:
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--headless')
+
+        result = adsl.get_proxy_list(True)[0]
+        temp = '--proxy-server=http://' + result
+        chrome_options.add_argument(temp)
+
         browser = webdriver.Chrome(chrome_options=chrome_options ,executable_path="/python/spiders/music2/chromedriver")
         browser.get("https://music.163.com/#/artist?id=12495660")
         time.sleep(8)
@@ -26,7 +27,7 @@ for i in range(10000):
         btn = browser.find_element_by_xpath('//*[@id="content-operation"]/a[1]')
         btn.click()
         print('success ',i)
-        time.sleep(10)
+        time.sleep(140 + random.randint(1,60))
         browser.close()
     except:
     	pass
